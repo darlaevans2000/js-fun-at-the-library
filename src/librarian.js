29accfc3e75f489d4ecef02d5233ec99baa6f5f8
darlaevans2000 <darlaevans2000@gmail.com>
2021-03-28 17:50:51 -0600

    Refactor librarian.js

--- src/librarian.js
+++ src/librarian.js
@@ -1,27 +1,27 @@
 class Librarian {
-  constructor(name, library){
+  constructor(name, library) {
     this.name = name;
     this.library = library;
     this.morning = true;
   }
   greetPatron(name, morning) {
-    if (morning){
+    if (morning) {
       return `Good morning, ${name}!`;
     } else {
       return `Hello, ${name}!`;
     }
   }
   findBook(title) {
-  for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
-    if (this.library.shelves.fantasy[i].title === title) {
-      this.library.shelves.fantasy.splice([i], 1);
-      return `Yes, we have ${title}`
+    for (var i = 0; i < this.library.shelves.fantasy.length; i++) {
+      if (this.library.shelves.fantasy[i].title === title) {
+        this.library.shelves.fantasy.splice([i], 1);
+        return `Yes, we have ${title}`
+      }
+    }
+    return `Sorry, we do not have ${title}`
   }
-}
-return `Sorry, we do not have ${title}`
-}
-calculateLateFee(days){
-  return Math.ceil(days * 0.25)
-}
-}
+  calculateLateFee(days) {
+    return Math.ceil(days * 0.25)
+  }
+};
 module.exports = Librarian;
