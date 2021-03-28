ddc1592ed9648104b0dc1af250b8c195d2e147fc
darlaevans2000 <darlaevans2000@gmail.com>
2021-03-28 17:46:55 -0600

    Refactor book.js

--- src/book.js
+++ src/book.js
@@ -1,37 +1,37 @@
 function createTitle(title) {
-      return `The ${title}`
-    };
+  return `The ${title}`
+};
 
 function buildMainCharacter(name, age, pronouns) {
-      return {
-        name: name,
-        age: age,
-        pronouns: pronouns,
-      }
-    };
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+};
 
 function saveReview(review, reviews) {
-      if (!reviews.includes(review)) {
-        reviews.push(review)
-      }
-    };
+  if (!reviews.includes(review)) {
+    reviews.push(review)
+  }
+};
 
 function calculatePageCount(bookTitle) {
-    return bookTitle.length * 20;
-    };
+  return bookTitle.length * 20;
+};
 
 function writeBook(title, mainCharacter, genre, pageCount) {
-      return book = {
-        title: title,
-        mainCharacter: mainCharacter,
-        pageCount: title.length * 20,
-        genre: genre,
-      }
-    };
-    
+  return book = {
+    title: title,
+    mainCharacter: mainCharacter,
+    pageCount: title.length * 20,
+    genre: genre,
+  }
+};
+
 function editBook(title) {
-      return title.pageCount = title.pageCount * .75
-    };
+  return title.pageCount = title.pageCount * .75
+};
 
 module.exports = {
   createTitle,
