564cd31fcc5f377a3f088ef35c381d1d3291b7f0
darlaevans2000 <darlaevans2000@gmail.com>
2021-03-28 17:50:03 -0600

    Refactor library.js

--- src/Library.js
+++ src/Library.js
@@ -1,4 +1,4 @@
-function createLibrary(name, shelves){
+function createLibrary(name, shelves) {
   return {
     name: name,
     shelves: {
@@ -9,8 +9,8 @@ function createLibrary(name, shelves){
   }
 };
 
-function addBook(library, book){
-  if (book.genre === "fantasy"){
+function addBook(library, book) {
+  if (book.genre === "fantasy") {
     return library.shelves.fantasy.push(book)
   } else if (book.genre === "fiction") {
     return library.shelves.fiction.push(book)
@@ -19,14 +19,14 @@ function addBook(library, book){
   }
 };
 
-function checkoutBook(library, title, genre){
-  for (var i = 0; i < library.shelves[genre].length; i++){
+function checkoutBook(library, title, genre) {
+  for (var i = 0; i < library.shelves[genre].length; i++) {
     if (library.shelves[genre][i].title === title) {
       library.shelves[genre].splice([i], 1);
       return `You have now checked out ${title} from the ${library.name}`;
     }
   }
-    return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
 };
 
 module.exports = {
